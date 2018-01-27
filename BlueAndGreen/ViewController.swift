9226a59dbb64423280889efa9093e481cf6c24c1
SIAMAK POURHABIB <seikendojo1@gmail.com>
2018-01-27 12:04:12 -0800

    initial commit

--- BlueAndGreen/ViewController.swift
+++ BlueAndGreen/ViewController.swift
@@ -10,14 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func blueBtnTapped(_ sender: Any) {
+      
+        colorView.backgroundColor = UIColor.blue
+        for i in 1...10 {
+            if (i % 2 != 0) {
+                print(" i = \(i)")
+               blueButton.setTitle("HolyMoly", for: .normal)
+
+            }
+        }
+        
+        func evenOrOdd(num: Int) {
+            if (num % 2 != 0) {
+                print("\(num)")
+            }else{
+                print("I am even")
+            }
+        }
+        
+        evenOrOdd(num: 10)
+    }
+    
+    @IBAction func greenBtnTapped(_ sender: Any) {
+        colorView.backgroundColor = UIColor.green
+        blueButton.setTitle("BLUE", for: .normal)
     }
 
 
